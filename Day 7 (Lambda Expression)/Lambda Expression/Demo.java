8b6c45121605e00beacfa2a0ad1714cc3fa72f7f
akashsingh24a@gmail.com <akashsingh24a@gmail.com>
2021-03-05 00:30:56 +0530

    activity

---
+++ Day 7 (Lambda Expression)/Lambda Expression/Demo.java
@@ -0,0 +1,29 @@
+@FunctionalInterface
+
+public interface Demo {
+
+ 
+
+	//case 3
+
+	public int add(int a, int b);
+
+	
+
+	//case 2
+
+	//public void sum(int a, int b);
+
+	
+
+	//Case 1
+
+	//public void diaplay();
+
+	default void show() {
+
+		System.out.println("I am default method");
+
+	}
+
+}
